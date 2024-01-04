5f22e526469e71fd0bead59ceb7f430fa372307d
duy-dk <khuongduying@gmail.com>
2024-01-04 14:44:13 +0700

    done tutorial

--- tutorial/spaCET.r
+++ tutorial/spaCET.r
@@ -106,24 +106,85 @@ ggsave(paste0(fig_path,"colocalization.png"), p4, width=4500, height=2000, unit=
 # Analyze the L-R network enrichment within ST spots
 
 ## calculate the L-R network score across ST spots.
-SpaCET_obj <- SpaCET.CCI.LRNetworkScore(SpaCET_obj,coreNo=8)
+SpaCET_obj <- SpaCET.CCI.LRNetworkScore(SpaCET_obj,coreNo=32)
 
 ## visualize the L-R network score.
 p5 <- SpaCET.visualize.spatialFeature(
-        SpaCET_obj, 
-        spatialType = "LRNetworkScore", 
-        spatialFeatures=c("Network_Score","Network_Score_pv")
-      )
+                      SpaCET_obj, 
+                      spatialType     = "LRNetworkScore", 
+                      spatialFeatures = c("Network_Score","Network_Score_pv")
+                    )
 ggsave(paste0(fig_path,"LRNetwork.png"), p5, width=4500, height=2000, unit="px", dpi=300)
 
 
 ## Ligand-Receptor analysis for a co-localized cell-type pair
-SpaCET_obj <- SpaCET.CCI.cellTypePair(SpaCET_obj, cellTypePair=c("CAF","Macrophage M2"))
+SpaCET_obj <- SpaCET.CCI.cellTypePair(
+                          SpaCET_obj, 
+                          cellTypePair = c("CAF","Macrophage M2")
+                          )
 ### [1] "CAF and Macrophage M2 have potential intercellular interaction in the current tissue."
 
 ## Visualize the interaction analysis of a co-localized cell-type pair.
-p6 <- SpaCET.visualize.cellTypePair(SpaCET_obj, cellTypePair=c("CAF","Macrophage M2"))
+p6 <- SpaCET.visualize.cellTypePair(
+                          SpaCET_obj, 
+                          cellTypePair = c("CAF","Macrophage M2")
+                          )
 
 ggsave(paste0(fig_path,"interaction_colocalized_celltype.png"), p6, width=5500, height=2000, unit="px", dpi=300)
 ###
 
+
+# Enrich cell-cell interactions at the tumor-immune interface
+# source("/mnt/rdisk/duydao/SPATIAL/GeneSmart/scripts/bin/interaction_duy.r")
+## Identify the Tumor-Stroma Interface
+SpaCET_obj <- SpaCET.identify.interface(SpaCET_obj)
+
+## Visualize the Interface
+p7 <- SpaCET.visualize.spatialFeature(
+                      SpaCET_obj, 
+                      spatialType     = "Interface", 
+                      spatialFeature  = "Interface"
+                      )
+ggsave(paste0(fig_path,"interface.png"), p7, width=3000, height=3000, unit="px", dpi=300)
+
+## Combine the interface and interaction spots
+## Import source code
+# source("/mnt/rdisk/duydao/SPATIAL/GeneSmart/scripts/bin/interaction_duy.r")
+
+SpaCET_obj <- SpaCET.combine.interface(SpaCET_obj, cellTypePair=c("CAF","Macrophage M2"))
+
+## Visualize the Interface. The spatialFeature should be formated as "Interface&celltype1_celltype2". Celltype 1 and 2 is in the order of alphabet.
+p8 <- SpaCET.visualize.spatialFeature(
+                      SpaCET_obj, 
+                      spatialType = "Interface", 
+                      spatialFeature = "Interface&CAF_Macrophage M2"
+                      )
+ggsave(paste0(fig_path,"interface_celltype.png"), p8, width=3000, height=3000, unit="px", dpi=300)
+
+## Compute the distance of CAF-M2 to tumor border
+p9 <- SpaCET.distance.to.interface(
+                      SpaCET_obj, 
+                      cellTypePair = c("CAF", "Macrophage M2")
+                      )
+ggsave(paste0(fig_path,"distance_CAF_M2_tumorBorder.png"), p9, width=3000, height=3000, unit="px", dpi=300)
+
+
+
+################################
+## Explore cancer cell states ##
+################################
+
+## further deconvolve malignant cell states
+SpaCET_obj <- SpaCET.deconvolution.malignant(SpaCET_obj, coreNo = 16)
+
+## show cancer cell state fraction of the first five spots
+SpaCET_obj@results$deconvolution$propMat[c("Malignant cell state A","Malignant cell state B"),1:6]
+
+## Visualize 
+p10 <- SpaCET.visualize.spatialFeature(
+  SpaCET_obj, 
+  spatialType = "CellFraction", 
+  spatialFeatures=c("Malignant","Malignant cell state A","Malignant cell state B"), 
+  nrow=1
+)
+ggsave(paste0(fig_path,"cancer_stages.png"), p10, width=6000, height=2000, unit="px", dpi=300)
